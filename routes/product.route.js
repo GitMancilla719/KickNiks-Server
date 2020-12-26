98a6f084fb7548932587765c58c1865ab80bd0a5
unknown <marwelmancilla95@gmail.com>
2020-12-26 21:46:41 +0800

    minor adjustments

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,14 +1,21 @@
 import express from 'express'
-import {getProducts, searchProducts, addProducts,updateProducts,deleteProducts} from '../controllers/product.controller.js'
+import {
+  getProducts,
+  searchProducts,
+  getOneProduct,
+  addProducts,
+  updateProducts,
+  deleteProducts,
+} from '../controllers/product.controller.js'
 import Authenticate from '../controllers/middlewares/authenticate.middleware.js'
 
 const ProductRouter = express.Router()
 
 ProductRouter.get('/', getProducts)
 ProductRouter.get('/item', searchProducts)
+ProductRouter.get('/item/:id', getOneProduct)
 ProductRouter.post('/addKicks', Authenticate, addProducts)
 ProductRouter.patch('/update/:id', Authenticate, updateProducts)
 ProductRouter.delete('/delete/:id', Authenticate, deleteProducts)
 
 export default ProductRouter
-
